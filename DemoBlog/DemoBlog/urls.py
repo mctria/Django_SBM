f5668e493dba0f2d362c6d9531a999ab2aa39610
Celcilin (Web Dev) <102462550+mctria@users.noreply.github.com>
2025-09-25 03:40:42 +0530

    new update

--- DemoBlog/DemoBlog/urls.py
+++ DemoBlog/DemoBlog/urls.py
@@ -21,7 +21,7 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    # path("__reload__/", include("django_browser_reload.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
 
     # blog
